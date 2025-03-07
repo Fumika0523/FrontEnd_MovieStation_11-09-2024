18fb746e48a1f6be6595e12bdc15e15880943259
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-03-08 01:36:01 +0900

    up

--- src/Components/HomeSreen/Header.jsx
+++ src/Components/HomeSreen/Header.jsx
@@ -11,19 +11,19 @@ function Header (){
 
          <div className='bgFade my-lg-4' >
         <div 
-        className="d-flex justify-content-center mt-sm-5 mb-5 pb-5 pt-sm-5  align-items-center flex-column  border-4"
+        className="d-flex justify-content-center mt-sm-4 mb-5 pb-5 pt-sm-5  align-items-center flex-column  border-4"
         >
         <div
-         className="text-white fw-bold my-lg-1  col-md-10 col-lg-8 col-sm-10  col-10" style={{fontSize:"4vw"}}
+         className="text-white fw-bold my-lg-1  col-md-10 col-lg-8 col-sm-10 fs-1 col-10" 
          >Your streaming guide for movies, TV shows & sport</div>
         <div
-         className='text-secondary col-md-10 col-lg-9 col-9' style={{fontSize:"2vw"}}
+         className='text-secondary col-md-10 col-lg-7 col-9 fs-4' 
          >Find where to stream new, popular & upcoming entertainment with JustWatch.</div>   
-        <div className=' col-lg-8 col-md-9  col-sm-10 col-10  d-flex  text-nowrap my-md-5 mt-3 d-flex justify-content-evenly' >
-        <Button variant="contained"  className="fw-bold text-nowrap rounded rounded-4"
-        style={{backgroundColor:"#f0ad4e",fontSize:"1.5vw",  overFlow: "hidden", textOverflow: "ellipsis",padding:"2% 5%"}}
+        <div className=' col-lg-8 col-md-9  col-sm-10 col-10  d-flex  text-nowrap my-md-4 mt-3 d-flex justify-content-evenly' >
+        <Button variant="contained"  className="fs-6 fw-bold text-nowrap rounded rounded-4"
+        style={{backgroundColor:"#f0ad4e", overFlow: "hidden", textOverflow: "ellipsis",padding:"2% 5%"}}
        >Discover Movies & TV shows</Button>
-        <Button variant="outlined" className="fw-bold rounded rounded-4 pfw-bold " style={{borderColor:"#adb5bd", color:"#adb5bd",fontSize:"1.5vw",padding:"2% 5%"}}  >Features</Button>
+        <Button variant="outlined" className="fw-bold rounded rounded-4 pfw-bold fs-6 " style={{borderColor:"#adb5bd", color:"#adb5bd",padding:"2% 5%"}}  >Features</Button>
         </div>
         {/* </div> */}
         </div>
