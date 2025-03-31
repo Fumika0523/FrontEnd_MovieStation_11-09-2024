b010f67beff9899ea84a2adc10b38ebe0aec153d
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-03-31 17:04:11 +0900

    up

--- src/Components/Order/OrderSummary.jsx
+++ src/Components/Order/OrderSummary.jsx
@@ -44,20 +44,32 @@ function OrderSummary({ mode }) {
         setLoading(true)
         let res = await axios.get(`${url}/order?sortBy=createdAt:desc`,config)
         console.log("GetSortedData",res.data.orderData)
-        setSortedData(red.data.orderData)
+        setSortedData(res.data.orderData)
     }
 
-    const totalOrderPrice = orderData.map((element) => {
-        const price = (element.movies).map((p) => {
-            //   console.log(p.amount) //250
-            return p.amount
+    //declearing
+    let totalOrderPrice ;
+    if(loading == true) // it has to display only 6 
+         {
+            console.log("loading",loading)
+         totalOrderPrice = sortedData.map((element) => {
+            const price = (element.movies).map((p) => {
+                return p.amount
+            })
+            const total = price.reduce((acc, cv) => acc + cv)
+            return total
         })
-        // console.log("price",price) // [250, 250]
-        const total = price.reduce((acc, cv) => acc + cv)
-        // console.log("total",total) //total 500
-        return total
-    })
-    // console.log("totalOrderPrice", totalOrderPrice) // [500,500,500,500]
+    }else{
+         totalOrderPrice = orderData.map((element) => {
+            const price = (element.movies).map((p) => {
+                return p.amount
+            })
+            const total = price.reduce((acc, cv) => acc + cv)
+            return total
+        })
+    }
+    
+console.log(totalOrderPrice)
 
     const renderTooltip = (props) => (
         <Tooltip id="button-tooltip" {...props}>
@@ -84,7 +96,36 @@ function OrderSummary({ mode }) {
                     {
                         // checking toggle
                         loading == true ?
-                        <h1>Sorted Order</h1>
+                        // <h1>Sorted Order</h1>
+                        totalOrderPrice.map((x) => (
+                            sortedData.map((element) => (
+                                <div key={element._id} className="mb-4">
+                                    <div className="d-flex mx-3  flex-row justify-content-between align-items-center my-2 ">
+                                        <div className="fs-6 fw-bold">Order ID : {element._id}</div>
+                                        <OverlayTrigger
+                                            placement="right"
+                                            delay={{ show: 250, hide: 400 }}
+                                            overlay={renderTooltip} >
+                                            <Button variant="none">
+                                                <MdDownloading className="fs-2"
+                                                style={{color:"rgb(46, 197, 54)"}} />
+                                            </Button>
+                                        </OverlayTrigger>
+                                    </div>
+                                    {
+                                        element.movies.map((movie) => (
+                                            <OrderSummaryCard key={movie._id} {...movie} updatedAt={element.updatedAt} />
+                                        ))}
+
+                                    {
+                                        <div className="text-end fw-bold fs-4 mx-2">Total Price :
+                                            <span className="fw-bold ms-1 fs-4">${x}</span> 
+                                        </div>
+                                    }
+                                    <hr />
+                                </div>
+                            ))
+                        ))
                         :
                          
                         totalOrderPrice.map((x) => (
